48b153a979f2ca9405bef38526b28f819f810824
martinmeza0 <martinmezacorreo@gmail.com>
2021-07-28 19:06:02 -0300

    add Modal with reactPortal, delete react-modal

--- src/components/Proyecto.jsx
+++ src/components/Proyecto.jsx
@@ -1,40 +1,38 @@
-import React, {useState} from 'react'
-import Modal from 'react-modal'
-import '../css/_Proyecto.sass'
-
-Modal.setAppElement('#root')
-
+import React, {useState} from 'react';
+import '../css/_Proyecto.sass';
+import Modal from './Modal';
 const Proyecto = (props) => {
     const {imagen, titulo, categorias, texto, linkPag, linkCode} = props 
     const [modalIsOpen, setModalIsOpen] = useState(false)
+    
     return (
         <>
             <div className="proyecto">
                 <img src={imagen} alt="" onClick={() => setModalIsOpen(true)}/>
             </div>
 
-            <Modal 
-                isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)} preventScroll={true} closeTimeoutMS={300}
-                className="Modal" overlayClassName="Overlay" 
-            >
-                <section className="modal-container">
-                    <div className="header">
-                        <h3 className="titulo">{titulo}</h3>
-                        <span className="borrar material-icons" onClick={() => setModalIsOpen(false)}>close</span>
-                    </div>
-                    <div className="body">
-                        <img src={imagen} alt=""/>  
-                        <p className="descripcion">{texto}</p>
-                        <div className="card-container">
-                            <p className="categorias">{categorias} </p>
-                            <div className="link">
-                                <a href={linkPag}>Demostracion <span className="material-icons">play_arrow</span></a>
-                                <a href={linkCode}>Codigo <span className="material-icons">code</span></a>
+            <Modal open={modalIsOpen}>
+                <div className="overlay">
+                    <div className="Modal">
+                        <section className="header">
+                            <h3 className="titulo">{titulo}</h3>
+                            <span className="borrar material-icons" onClick={() => setModalIsOpen(false)}>close</span>
+                        </section>
+                        <section className="body">
+                            <img src={imagen} alt=""/>  
+                            <p className="descripcion">{texto}</p>
+                            <div className="card-container">
+                                <p className="categorias">{categorias} </p>
+                                <div className="link">
+                                    <a href={linkPag}>Demostracion <span className="material-icons">play_arrow</span></a>
+                                    <a href={linkCode}>Codigo <span className="material-icons">code</span></a>
+                                </div>
                             </div>
-                        </div>
+                        </section>
                     </div>
-                </section>
+                </div>
             </Modal>
+
         </>
     )
 }
