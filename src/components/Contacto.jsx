a29ec46843fd2d1b7a45e99b1cb96fcbf9c2bd23
martinmeza0 <martinmezacorreo@gmail.com>
2021-07-31 17:29:38 -0300

    commit before i shit this

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {useForm} from 'react-hook-form';
 import '../css/_Contacto.sass';
 import emailjs from 'emailjs-com';
@@ -6,6 +6,8 @@ import Modal from './Modal';
 
 const Contacto = () => {
     const { register, handleSubmit, formState: { errors } } = useForm()
+    const [modalIsOpen, setModalIsOpen] = useState(false)
+
     
     const enviarMensaje  = (data,e) => {   
         let Datos = {
@@ -13,13 +15,20 @@ const Contacto = () => {
             email: data.Correo,
             message: data.Mensaje
         }
-        emailjs.send("service_zhua4wc","template_spqer1u", Datos, "user_HPt4N1jpTCSwJcAv87Vgq")
-            .then(function(response) {
-                console.log('SUCCESS!', response.status, response.text);
-            }, function(error) {
-                console.log('FAILED...', error);
-            });
+        // emailjs.send("service_zhua4wc","template_spqer1u", Datos, "user_HPt4N1jpTCSwJcAv87Vgq")
+            // .then(function(response) {
+        // setTimeout(setModalIsOpen(true), 5000)
+        setTimeout = (() => {
+            setModalIsOpen(true)
+        }, 400)
         e.target.reset();
+    
+        // } 
+        // setModalIsOpen(false)
+        // return;
+            // }, function(error) {
+                // console.log('FAILED...', error);
+            // });
     }
     return (
         <>
@@ -73,8 +82,11 @@ const Contacto = () => {
                         />
                     </form>  
             </section>  
-            <Modal>
-
+            <Modal open={modalIsOpen}>
+                <div className="pop-up">
+                    <img src="/assets/svg/undraw_Mail_sent_green.svg" alt="" />
+                    <p> ¡El mensaje se envió correctamente! </p>
+                </div>
             </Modal>
         </>
     )
