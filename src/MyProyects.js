17599cac1777c09a643e71569ae99a26dce781ba
Martinmeza0 <martinmezacorreo@gmail.com>
2022-03-18 20:50:41 -0300

    feat: information of each proyect in array for better organization

---
+++ src/MyProyects.js
@@ -0,0 +1,40 @@
+export const MyProyects = [{
+  img: '/assets/img/Luxury Cars.jpg',
+  title: 'Luxury Cars - The cars ecommerce',
+  technology: 'REACT - STYLED COMPONENTS',
+  description: 'This is a frontend multi-page application of a vehicle shop website <br>Built around <b>react-router-dom </b> with the Google Maps API. <br/><br/>The proyect have four pages to visit:<br/> <br/><ul><li>First, the main page with the list of cars to buy.</li><li>Second, a detail page of each cars and a carousel of images.</li><li>Third, an informative or faq page.</li><li>And lastly, a page in maintenance to simulate that the page is in development.</li></ul><br>This proyect is full responsive design, with reusables components like the carousel.',
+  linkDemo: 'https://github.com/martinmeza0/luxurycars',
+  linkCode: 'https://martinmeza0.github.io/luxurycars/'
+},
+{
+  img: '/assets/img/Martin Meza portafolio.jpg',
+  title: 'Martin Meza portafolio',
+  technology: 'SCSS - REACT',
+  description: 'My Frontend portafolio, built in a minimalistic style without overflow with too much information because my work speak by itself. <br> I implemented the library <b>react-hook-form</b> for the contact form and <b>Emailjs</b> for the funcionality of the email. <br> With <b> React Portal function </b> i built a modal to display the information of each project in a easy way. <br>Lastly, i deployed the proyect with firebase service.',
+  linkDemo: '',
+  linkCode: 'https://github.com/martinmeza0/mi-portafolio'
+},
+{
+  img: '/assets/img/25Alfajores.jpg',
+  title: '25Alfajores Grocery Store',
+  technology: 'HTML - SASS - JS',
+  description: 'This is a single web aplication created for a grocery store in my town. <br> This proyect have a menu restaurant and a carousel of images. <br> I used libraries like <b>SimpleParallax.js</b> for the parallax effect inthe header And <b>Glider.js</b> for the image slider. <br>Lastly, is full responsive design.',
+  linkDemo: 'https://martinmeza0.github.io/blog-comida/',
+  linkCode: 'https://github.com/martinmeza0/blog-comida'
+},
+{
+  img: '/assets/img/Agenda de contacto.jpg',
+  title: 'Contact schedule',
+  technology: 'HTML - CSS - JS',
+  description: 'This is a schedule for add contacts by their names, phone and address. <br> Built around the library <b>SweetAlert2</b>, for a beautiful alert message. <br>Also, the information won\'t be deleted after refreshing the page, because is stored in the cache. <br> The proyect is full responsive design.',
+  linkDemo: 'https://martinmeza0.github.io/agenda-contacto/',
+  linkCode: 'https://github.com/martinmeza0/agenda-contacto'
+},
+{
+  img: '/assets/img/Juego de memoria.jpg',
+  title: 'Memory Game',
+  technology: 'HTML - CSS - JS',
+  description: 'A memory card game with counts of bads and successful plays. <br> When the player press the button the game start and the chronometer start to count until the player win the game. <br> The project is full responsive design.',
+  linkDemo: 'https://martinmeza0.github.io/memory-game/',
+  linkCode: 'https://github.com/martinmeza0/memory-game'
+}]
