f98578ae60f84f63b2ba4949c9ddbf458a77835b
Martinmeza0 <martinmezacorreo@gmail.com>
2022-03-18 09:30:50 -0300

    feat: new component to render all proyects dynamically instead of rendering manually in app.jsx

---
+++ src/components/ProyectList/ProyectList.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Proyect from '../Proyect/Proyect'
+import { MyProyects } from '../../MyProyects'
+import './ProyectList.sass'
+
+const ProyectList = () => {
+  return (
+    <>
+      <section className='container' />
+    </>
+  )
+}
+
+export default ProyectList
